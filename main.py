69b77148b61426af351a86ad73fd87ed45b32fb4
JKafak97 <dr.jara1@seznam.cz>
2020-12-11 21:21:23 +0100

    function to find links add

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import webbrowser
 
 def main():
     request = get_link()
-    url = get_soup(request)
+    url, soup = get_soup(request)
     web_open(url)
+    links_find(soup)
 
 
 def get_link():
@@ -19,11 +20,17 @@ def get_soup(request):
     soup = BeautifulSoup(request.content, 'html.parser')
     title = soup.find(class_ = "firstHeading").text
     url = 'https://en.wikipedia.org/wiki/%s' %title
-    return url
+    return url, soup
     
 
 def web_open(url):  
     webbrowser.open(url)
+
+
+def links_find(soup):
+    all_links= soup.findAll('a')
+    for link in all_links:
+        print (link)
 
 
 if __name__ == "__main__":
