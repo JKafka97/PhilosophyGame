34e1d5be164150e8e2e53100aeb54c47374416c4
JKafak97 <dr.jara1@seznam.cz>
2020-12-11 21:15:20 +0100

    first structure

--- main.py
+++ main.py
@@ -3,11 +3,28 @@ from bs4 import BeautifulSoup
 import webbrowser
 
 
-while True:
+def main():
+    request = get_link()
+    url = get_soup(request)
+    web_open(url)
+
+
+def get_link():
     a = "https://en.wikipedia.org/wiki/Special:Random"
-    u = requests.get(a)
-    soup = BeautifulSoup(u.content, 'html.parser')
+    request = requests.get(a)
+    return request
+
+
+def get_soup(request):
+    soup = BeautifulSoup(request.content, 'html.parser')
     title = soup.find(class_ = "firstHeading").text
     url = 'https://en.wikipedia.org/wiki/%s' %title
+    return url
+    
+
+def web_open(url):  
     webbrowser.open(url)
-    break
+
+
+if __name__ == "__main__":
+    main()
