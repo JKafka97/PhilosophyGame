fb1bbc0336a588baee7484d5fb298cb73a6ef3d5
JKafak97 <dr.jara1@seznam.cz>
2020-12-13 18:29:31 +0100

    all functions works

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import webbrowser
+import random
 
 
 def main():
     request = get_link()
     url, soup = get_soup(request)
     web_open(url)
-    links_find(soup)
+    while "Philosophy" not in url:
+        url, soup = links_find(soup)
+        web_open(url)
 
 
 def get_link():
@@ -28,10 +31,19 @@ def web_open(url):
 
 
 def links_find(soup):
-    links_list = soup.find_all("a")
-    for link in links_list:
-        if "href" in link.attrs:
-            print(str(link.attrs["href"]))
+    rnd_num = random.randint(3,5)
+    for link in (soup.findAll('a'))[rnd_num:]:
+            try:
+                if "." not in (list(link.get('href')))[-5:] :
+                    next_link = link.get('href')
+                    if "404" in next_link or "searchInput" in next_link or "org" in next_link:
+                        continue
+                    else: break
+            except: continue
+    url = "https://en.wikipedia.org//" + next_link
+    getr = requests.get(url)
+    soup = BeautifulSoup(getr.text, 'html.parser')
+    return url, soup
 
 
 if __name__ == "__main__":
