af27f1fec2dc909d52a546fb8c371432ecc3a88d
JKafak97 <dr.jara1@seznam.cz>
2020-12-27 19:27:57 +0100

    version 2.0

--- main.py
+++ main.py
@@ -1,49 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
 import webbrowser
-import random
+from selenium import webdriver
+import urllib
 
+start_link = "https://en.wikipedia.org/wiki/Special:Random"
+target_link = "https://en.wikipedia.org/wiki/Philosophy"
+article_chain = [start_link]
 
 def main():
-    request = get_link()
-    url, soup = get_soup(request)
-    web_open(url)
-    while "Philosophy" not in url:
-        url, soup = links_find(soup)
-        web_open(url)
+    PATH = input("Insert the absolute path to your Chromedriver: \nOr download it on https://chromedriver.chromium.org/downloads.")
+    driver = driver_set(PATH)
+    driver.get(start_link)
+    my_counter = 1
+    while article_chain[-1] != target_link:   
+        first_link = links_find(article_chain[-1])
+        driver.get(first_link)
+        if first_link in article_chain:
+            first_link = start_link
+            print("Sorry you are in loop")
+        article_chain.append(first_link)
+        my_counter += 1
+    print("You found Philosophy after {} try".format(my_counter))
+    driver.close()
 
 
-def get_link():
-    a = "https://en.wikipedia.org/wiki/Special:Random"
-    request = requests.get(a)
-    return request
+def driver_set(PATH):
+    driver = webdriver.Chrome(PATH) 
+    return driver
 
 
-def get_soup(request):
-    soup = BeautifulSoup(request.content, 'html.parser')
-    title = soup.find(class_ = "firstHeading").text
-    url = 'https://en.wikipedia.org/wiki/%s' %title
-    return url, soup
-    
-
-def web_open(url):  
-    webbrowser.open(url)
-
-
-def links_find(soup):
-    rnd_num = random.randint(3,5)
-    for link in (soup.findAll('a'))[rnd_num:]:
-            try:
-                if "." not in (list(link.get('href')))[-5:] :
-                    next_link = link.get('href')
-                    if "404" in next_link or "searchInput" in next_link or "org" in next_link:
-                        continue
-                    else: break
-            except: continue
-    url = "https://en.wikipedia.org//" + next_link
-    getr = requests.get(url)
-    soup = BeautifulSoup(getr.text, 'html.parser')
-    return url, soup
+def links_find(url):
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    content_div = soup.find(
+        id="mw-content-text").find(class_="mw-parser-output")
+    article_link = None
+    for element in content_div.find_all("p", recursive=False):
+        if element.find("a", recursive=False):
+            article_link = element.find("a", recursive=False).get('href')
+            break
+    if not article_link:
+        return
+    first_link = urllib.parse.urljoin('https://en.wikipedia.org/', article_link)
+    return first_link
 
 
 if __name__ == "__main__":
