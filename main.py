c9637472eea3994fddfd2f9826ba74ae53a88483
JKafka <dr.jara1@seznam.cz>
2021-01-10 12:18:24 +0100

    version 3.0

--- main.py
+++ main.py
@@ -4,24 +4,32 @@ import webbrowser
 from selenium import webdriver
 import urllib
 
+
 start_link = "https://en.wikipedia.org/wiki/Special:Random"
 target_link = "https://en.wikipedia.org/wiki/Philosophy"
-article_chain = [start_link]
+article_chain = []
 
 def main():
     PATH = input("Insert the absolute path to your Chromedriver: \nOr download it on https://chromedriver.chromium.org/downloads.")
     driver = driver_set(PATH)
     driver.get(start_link)
-    my_counter = 1
-    while article_chain[-1] != target_link:   
-        first_link = links_find(article_chain[-1])
-        driver.get(first_link)
-        if first_link in article_chain:
-            first_link = start_link
-            print("Sorry you are in loop")
-        article_chain.append(first_link)
-        my_counter += 1
-    print("You found Philosophy after {} try".format(my_counter))
+    my_counters = {"total": 1, "reset": 0}
+    while driver.current_url != target_link:  
+        first_link = links_find(driver.current_url)
+        try:
+            click_link = driver.find_element_by_xpath('//a[@href="'+first_link+'"]')
+            click_link.click()
+            if first_link in article_chain:
+                print("Sorry, you are in loop.")
+                my_counters["reset"] += 1
+                driver.get(start_link)
+            my_counters["total"] += 1
+            article_chain.append(first_link)
+        except: 
+            print("Sorry, there is no link.")
+            my_counters["reset"] += 1
+            driver.get(start_link)
+    print("You found Philosophy after {} tries and {} restarts.".format(my_counters["total"], my_counters["reset"]))
     driver.close()
 
 
@@ -37,14 +45,15 @@ def links_find(url):
     content_div = soup.find(
         id="mw-content-text").find(class_="mw-parser-output")
     article_link = None
-    for element in content_div.find_all("p", recursive=False):
-        if element.find("a", recursive=False):
-            article_link = element.find("a", recursive=False).get('href')
-            break
-    if not article_link:
-        return
-    first_link = urllib.parse.urljoin('https://en.wikipedia.org/', article_link)
-    return first_link
+    try:
+        for element in content_div.find_all("p", recursive=False):
+            if element.find("a", recursive=False):
+                article_link = element.find("a", recursive=False).get('href')
+                break
+        if not article_link:
+            return
+        return article_link
+    except: return
 
 
 if __name__ == "__main__":
