a480ba43e16794a51e2bcc43985e90a4237aa6be
JKafka <dr.jara1@seznam.cz>
2023-09-15 12:51:15 +0200

    Visual changes

--- main.py
+++ main.py
@@ -1,60 +1,52 @@
-import requests
 from bs4 import BeautifulSoup
-import webbrowser
+from selenium.webdriver.common.by import By
 from selenium import webdriver
-import urllib
-
 
 start_link = "https://en.wikipedia.org/wiki/Special:Random"
 target_link = "https://en.wikipedia.org/wiki/Philosophy"
-article_chain = []
+
+def find_first_article_link(driver):
+    try:
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        for element in soup.select("#mw-content-text .mw-parser-output p a[href^='/wiki/']"):
+            return "https://en.wikipedia.org" + element.get("href")
+    except:
+        pass
+    return None
+
+def configure_webdriver():
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    return webdriver.Chrome(options=options)
+
+def link_check(restarts, total_attempts, article_chain, first_link, driver):
+    if first_link:
+        if first_link in article_chain:
+            print("Sorry, you are in a loop.")
+            restarts += 1
+            driver.get(start_link)
+            article_chain.clear()
+        else:
+            driver.get(first_link)
+            article_chain.append(first_link)
+        total_attempts += 1    
+    else:
+        print("Sorry, there is no link.")
+        restarts += 1
+        driver.get(start_link)
+        article_chain.clear()
+    return restarts, total_attempts
 
 def main():
-    PATH = input("Insert the absolute path to your Chromedriver: \nOr download it on https://chromedriver.chromium.org/downloads.")
-    driver = driver_set(PATH)
-    driver.get(start_link)
-    my_counters = {"total": 1, "reset": 0}
-    while driver.current_url != target_link:  
-        first_link = links_find(driver.current_url)
-        try:
-            click_link = driver.find_element_by_xpath('//a[@href="'+first_link+'"]')
-            click_link.click()
-            if first_link in article_chain:
-                print("Sorry, you are in loop.")
-                my_counters["reset"] += 1
-                driver.get(start_link)
-            my_counters["total"] += 1
-            article_chain.append(first_link)
-        except: 
-            print("Sorry, there is no link.")
-            my_counters["reset"] += 1
-            driver.get(start_link)
-    print("You found Philosophy after {} tries and {} restarts.".format(my_counters["total"], my_counters["reset"]))
-    driver.close()
-
-
-def driver_set(PATH):
-    driver = webdriver.Chrome(PATH) 
-    return driver
-
-
-def links_find(url):
-    response = requests.get(url)
-    html = response.text
-    soup = BeautifulSoup(html, "html.parser")
-    content_div = soup.find(
-        id="mw-content-text").find(class_="mw-parser-output")
-    article_link = None
-    try:
-        for element in content_div.find_all("p", recursive=False):
-            if element.find("a", recursive=False):
-                article_link = element.find("a", recursive=False).get('href')
-                break
-        if not article_link:
-            return
-        return article_link
-    except: return
-
+    restarts = 0
+    total_attempts = 0
+    article_chain = []
+    driver = configure_webdriver()
+    while driver.current_url != target_link:
+        first_link = find_first_article_link(driver)
+        restarts, total_attempts = link_check(restarts, total_attempts, article_chain, first_link, driver)   
+    print(f"You found Philosophy after {total_attempts} tries and {restarts} restarts.")
+    driver.quit()
 
 if __name__ == "__main__":
     main()
